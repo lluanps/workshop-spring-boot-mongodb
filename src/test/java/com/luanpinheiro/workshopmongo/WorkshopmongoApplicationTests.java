f14bb0e1ab284babbb74d81524eef65e404a6139
Luan Pinheiro <lluanps@gmail.com>
2022-12-20 22:52:35 -0300

    project created

---
+++ src/test/java/com/luanpinheiro/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanpinheiro.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
